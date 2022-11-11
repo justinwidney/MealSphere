230f4e642173ea9d31b2b47790c11d77b680168a
justinwidney <jwidney@ualberta.ca>
2022-11-11 15:34:53 -0700

    init project

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(
+  "/grapgql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+      events: [String!]! 
+
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+
+    }
+    
+    schema{
+      query: RootQuery
+      mutation: RootMutation
+    }`),
+    rootValue: {
+      events: () => {
+        return ["Romantic Cooking", "Sailing", "Que"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
+
+app.listen(3000);
